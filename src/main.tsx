f1a761020686b8da3de53d0b73765551aaea984b
OWLsTyLe <maxshypilkin@gmail.com>
2026-06-10 21:21:31 +0300

    full

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/React-lab/'>
       <App />
     </BrowserRouter>
   </Provider>
