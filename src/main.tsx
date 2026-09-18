2fe40d3ddae814c1dc84ce8bf260df68073f78b2
OWLsTyLe <162433879+OWLsTyLe@users.noreply.github.com>
2026-09-18 21:31:12 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter basename='/React-lab/'>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </Provider>
